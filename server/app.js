323fc9361e9eff1b671cad0451c734cd6214301d
Manish <mks25022025@gmail.com>
2025-09-11 07:11:41 +0530

    fixed server

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import { User } from './models/user.js'
 
 dotenv.config()
 mongoose.connect(process.env.conString)
@@ -26,13 +27,15 @@ app.get('/', async (req, res) => {
 app.post('/signup', async (req, res) => {
     let data = req.body
 
-    let isPresent = await db.collection('users').findOne({email: data.email})
+    // let isPresent = await db.collection('users').findOne({email: data.email})
+    let isPresent = await User.find({email: data.email})
 
     if(isPresent) {
         res.status(400).send('Email already exists!')
     } else {
-        await db.collection('users').insertOne({username: data.username, email: data.email, password: data.password})
-        res.send(data);
+        // await db.collection('users').insertOne({username: data.username, email: data.email, password: data.password})
+        let newDoc = await User.create({username: data.username, email: data.email, password: data.password})
+        res.send(newDoc);
     }
 })
 
@@ -51,6 +54,10 @@ app.post('/login', async (req, res) => {
     } else {
         return res.status(400).send('Invalid email!');
     }
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 })
 
 export default app
