c85ce4c88382c1801c490944e86d6bb49166d6f4
Manish <mks25022025@gmail.com>
2025-09-12 19:56:30 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -24,35 +24,35 @@ app.get('/', async (req, res) => {
 
 app.post('/signup', async (req, res) => {
     let data = req.body
-
-    // let isPresent = await db.collection('users').findOne({email: data.email})
     let isPresent = await User.find({ email: data.email })
-
-    if (isPresent) {
-        res.status(400).send('Email already exists!')
-    } else {
-        // await db.collection('users').insertOne({username: data.username, email: data.email, password: data.password})
-        let newDoc = await User.create({ username: data.username, email: data.email, password: data.password })
-        res.send(newDoc);
-    }
-})
-
-app.post('/login', async (req, res) => {
-    let data = req.body
-
-    let emailFound = await db.collection('users').findOne({ email: data.email })
-    let passwordFound = await db.collection('users').findOne({ password: data.password })
-
-    if (emailFound) {
-        if (passwordFound) {
-            return res.send(emailFound);
+    try {
+        if (isPresent.length > 0) {
+            res.status(400).send('Email already exists!')
         } else {
-            return res.status(400).send('Invalid password!');
+            let newDoc = await User.create({ username: data.username, email: data.email, password: data.password })
+            res.send(newDoc);
         }
-    } else {
-        return res.status(400).send('Invalid email!');
+    } catch (error) {
+        res.status(500).json({message: 'Internal Server Error', error: error.message})
     }
 })
+
+// app.post('/login', async (req, res) => {
+//     let data = req.body
+
+//     let emailFound = await db.collection('users').findOne({ email: data.email })
+//     let passwordFound = await db.collection('users').findOne({ password: data.password })
+
+//     if (emailFound) {
+//         if (passwordFound) {
+//             return res.send(emailFound);
+//         } else {
+//             return res.status(400).send('Invalid password!');
+//         }
+//     } else {
+//         return res.status(400).send('Invalid email!');
+//     }
+// })
 
 // app.listen(process.env.PORT || 3000, () => {
 //     console.log(`Server is running on port ${process.env.PORT}`);
