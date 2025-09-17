5be9a2d6fc5753551704ef083cfa7b8b3cd5304f
Manish <mks25022025@gmail.com>
2025-09-17 11:42:05 +0530

    Added log out button

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -19,6 +19,7 @@ const Navbar = () => {
                     { !userData && <NavLink className={(e) => `flex justify-center items-center px-4 py-1 box-border rounded-md border-2 ${e.isActive ? "border-block" : "border-transparent"}`} to="/signup">Sign Up</NavLink>}
                     { !userData && <NavLink className={(e) => `flex justify-center items-center px-4 py-1 box-border rounded-md border-2 ${e.isActive ? "border-block" : "border-transparent"}`} to="/login">Log In</NavLink>}
                     { userData ? <NavLink className={(e) => `flex justify-center items-center px-4 py-1 box-border rounded-md border-2 ${e.isActive ? "border-block" : "border-transparent"}`} to="/profile">Profile</NavLink> : ""}
+                    {userData ? <button className='px-4 py-1 box-border rounded-md border-2 border-transparent hover:border-block text-red-400 font-bold' onClick={() => { localStorage.removeItem('token'); window.location.href = '/' }}>Log Out</button> : "" }
                 </div>
                 <img src={menu} className='block md:hidden w-10 invert' onClick={() => setBar(true)} />
                 { bar && <div className="box-border flex flex-col md:hidden px-10 py-5 fixed top-15 right-10 justify-center rounded-xl items-center border border-gray-400 backdrop-blur-sm bg-black/30 shadow-md shadow-gray-600 text-white z-20">
@@ -29,6 +30,7 @@ const Navbar = () => {
                     { !userData && <NavLink className={(e) => `px-4 py-1 box-border rounded-md ${e.isActive ? "text-blue-300" : "text-white"}`} onClick={()=>setBar(false)} to="/signup">Sign Up</NavLink>}
                     { !userData && <NavLink className={(e) => `px-4 py-1 box-border rounded-md ${e.isActive ? "text-blue-300" : "text-white"}`} onClick={()=>setBar(false)} to="/login">Log In</NavLink>}
                     { userData ? <NavLink className={(e) => `px-4 py-1 box-border rounded-md ${e.isActive ? "text-blue-300" : "text-white"}`} onClick={()=>setBar(false)} to="/profile">Profile</NavLink> : ""}
+                    {userData ? <button className='mt-2 px-4 py-1 box-border rounded-md border-2 border-transparent text-red-400 font-bold' onClick={() => { localStorage.removeItem('token'); window.location.href = '/' }}>Log Out</button> : "" }
                 </div>}
             </nav>
         </>
